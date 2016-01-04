a1ee3b1ca8888d2b99cf20033b0c98c6535ec3f8
snapcube <taccolaaless@gmail.com>
2016-01-04 23:57:40 +0100

    Done FlagTrait

--- app/traits/FlagTrait.php
+++ app/traits/FlagTrait.php
@@ -1,4 +1,22 @@
 <?php
 trait FlagTrait {
-    //TODO
+    public $flagged = false;
+
+    public function isFlagged(){
+        return $this->isFlagged();
+    }
+
+    public function flag(){
+        return $this->setFlagged(true);
+    }
+
+    public function unFlag(){
+        return $this->setFlagged(false);
+    }
+
+    public function setFlagged(bool $flagged): Model{
+        $this->flagged = $flagged;
+        return $this;
+    }
+
 }
